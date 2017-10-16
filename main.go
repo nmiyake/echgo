7799802bb82db52e99dda67edf9c98333b28fca3
Nick Miyake <nmiyake@palantir.com>
2017-10-16 15:30:45 -0700

    Add version variable and define build configuration

--- main.go
+++ main.go
@@ -1,14 +1,22 @@
 package main
 
 import (
+	"flag"
 	"fmt"
-	"os"
 	"strings"
 
 	"github.com/nmiyake/echgo/echo"
 )
 
+var version = "none"
+
 func main() {
+	versionVar := flag.Bool("version", false, "print version")
+	flag.Parse()
+	if *versionVar {
+		fmt.Println("echgo version:", version)
+		return
+	}
 	echoer := echo.NewEchoer()
-	fmt.Println(echoer.Echo(strings.Join(os.Args[1:], " ")))
+	fmt.Println(echoer.Echo(strings.Join(flag.Args(), " ")))
 }
